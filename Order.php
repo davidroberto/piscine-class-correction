941761b3b2172604cb9b866e3ffe4b181fab4e05
David Robert <contact@david-robert.fr>
2024-11-07 15:08:05 +0100

    exception

--- Order.php
+++ Order.php
@@ -4,9 +4,10 @@
 class Order {
     public $id;
     public $customerName;
-    public $status = "cart";
+    public $status;
     public $totalPrice;
     public $products = [];
+    public $shippingAddress;
 
 
     // le constructeur est une méthode "magique"
@@ -15,6 +16,8 @@ class Order {
     // pour cette classe
     // un objet créé pour une classe est appelée "instance de class"
     public function __construct($customerName) {
+        $this->status === 'cart';
+        $this->totalPrice = 0;
         $this->customerName = $customerName;
         $this->id = uniqid();
     }
@@ -29,9 +32,39 @@ class Order {
         }
     }
 
+    public function removeProduct() {
+
+        if ($this->status === "cart" && !empty($this->products)) {
+            array_pop($this->products);
+            $this->totalPrice -= 3;
+        }
+    }
+
+    public function setShippingAddress($shippingAddress) {
+        if ($this->status === "cart") {
+            $this->shippingAddress = $shippingAddress;
+            $this->status === "shippingAddressSet";
+        }
+    }
+
     public function pay() {
-        if($this->status === "cart") {
+        if($this->status === "shippingAddressSet" && !empty($this->products)) {
             $this->status = "paid";
+        } else {
+            // si le paiement ne peux pas être fait
+            // on lève une exception, c'est à dire on déclenche une erreur
+            // que l'on peut récupérer ensuite pour l'afficher dans le HTML
+            throw new Exception('Vous ne pouvez pas payer, merci de remplir votre adresse d\'abord');
+        }
+
+    }
+
+
+    public function ship() {
+        if ($this->status === 'paid') {
+            $this->status = "shipped";
+        } else {
+            throw new Exception("La commande ne peux pas être expédiée. elle n'est pas encore payée");
         }
     }
 }
@@ -42,15 +75,9 @@ class Order {
 // je créé une instance de la classe Order
 // c'est à dire un objet issu du plan de construction de la classe Order
 $order1 = new Order("David Robert");
-$order1->addProduct();
-$order1->addProduct();
-$order1->addProduct();
 
-$order2 = new Order();
-$order2->addProduct();
+$order1->ship();
 
-
-$order1->pay();
 
 
 
